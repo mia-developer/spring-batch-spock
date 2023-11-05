633769a3d93ffff0189dd36f11ceb1e99d3366cf
mia-developer <office.yeon@gmail.com>
2023-11-06 01:06:40 +0900

    feat: atomic integer

--- src/main/java/org/example/sample/step/SampleItemReader.java
+++ src/main/java/org/example/sample/step/SampleItemReader.java
@@ -4,6 +4,7 @@ import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
 
 import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
 import lombok.extern.slf4j.Slf4j;
 import org.example.sample.enums.SampleType;
 import org.example.sample.model.Sample;
@@ -15,11 +16,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class SampleItemReader implements ItemReader<Sample> {
 
-	private int count = 0;
+	private final AtomicInteger count = new AtomicInteger(0);
 
 	@Override
 	public Sample read() {
-		if (count++ > 10){
+		if (count.incrementAndGet() > 10){
 			return null;
 		}
 
